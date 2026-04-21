ec22a2843a128f660e3e066e87ee1d3472b91afd
Desislava Ivanova <desislavai366@gmail.com>
2026-04-21 12:35:37 +0300

    Update DatabaseSeeder.php

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -1,5 +1,5 @@
-<?php
 
+php<?php
 namespace Database\Seeders;
 
 use App\Models\User;
@@ -10,12 +10,12 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            RoleSeeder::class,
+            CategorySeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
